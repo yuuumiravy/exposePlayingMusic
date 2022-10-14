46a97a1cdafd0b8851298843a143e1b3a0c5026c
yuuumbk <hikoki.y@gmail.com>
2022-10-14 18:56:28 +0900

    add: react-native-elements
    install react-native-elements
    uninstall react-native-paper

--- App.jsx
+++ App.jsx
@@ -1,17 +1,10 @@
 import {NavigationContainer} from '@react-navigation/native';
-import {Provider as PaperProvider} from 'react-native-paper';
-// import TabNavigator from './navigations/TabNavigator';
-import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
 import TabNavigator from './navigations/TabNavigator';
-
-const Tab = createBottomTabNavigator();
 
 export default function App() {
   return (
     <NavigationContainer>
-      {/* <PaperProvider> */}
       <TabNavigator />
-      {/* </PaperProvider> */}
     </NavigationContainer>
   );
 }
